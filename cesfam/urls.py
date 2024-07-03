aebfb84c67d50e8b55ade6fc5c02c366528e31a1
o0leslyth40o <102860687+o0leslyth40o@users.noreply.github.com>
2024-07-03 18:53:51 -0400

    agregue urls farmacia, login, sign_in

--- cesfam/urls.py
+++ cesfam/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path, include
-from .views import home
+from .views import home, login, farmacia, sign_in
 
 
 
 urlpatterns = [
     path('', home, name= "home"),
+    path('login/', login, name="login"),
+    path('farmacia/', farmacia, name="farmacia"),
+    path('sign_in/', sign_in, name="sign_in"),
 ]
