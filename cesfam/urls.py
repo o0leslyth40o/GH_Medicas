6c78e40d29bcbc707d82f3351e866d59c70d13ee
o0leslyth40o <102860687+o0leslyth40o@users.noreply.github.com>
2024-07-05 15:10:57 -0400

    agregue ruta farmacia y defini views farmacia

--- cesfam/urls.py
+++ cesfam/urls.py
@@ -1,11 +1,9 @@
 from django.urls import path
-from .views import home, login, farmacia, sign_in
+from .views import home, farmacia
 
 
 # defini la ruta.
 urlpatterns = [
     path('', home, name= "home"),
-    path('login/', login, name="login"),
     path('farmacia/', farmacia, name="farmacia"),
-    path('sign_in/', sign_in, name="sign_in"),
 ]
--- cesfam/views.py
+++ cesfam/views.py
@@ -4,3 +4,6 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'cesfam/home.html')
+
+def farmacia(request):
+    return render(request, 'cesfam/farmacia.html')
