b889c8eefb347ed17bbc1462409f3bf08c351710
o0leslyth40o <102860687+o0leslyth40o@users.noreply.github.com>
2024-07-05 22:20:11 -0400

    importe tabla especialidad

--- cesfam/admin.py
+++ cesfam/admin.py
@@ -1,7 +1,5 @@
 from django.contrib import admin
-from .models import medico, especialidad, paciente
+from .models import especialidad
 
 # Register your models here.
-admin.site.register(medico)
 admin.site.register(especialidad)
-admin.site.register(paciente)
