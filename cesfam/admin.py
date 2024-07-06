a626e3c635a08d3b5b3f521fce67cff16b60a8a1
o0leslyth40o <102860687+o0leslyth40o@users.noreply.github.com>
2024-07-05 22:16:57 -0400

    cree forms.py y la primera tabla

--- cesfam/admin.py
+++ cesfam/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import medico, especialidad, paciente
 
 # Register your models here.
+admin.site.register(medico)
+admin.site.register(especialidad)
+admin.site.register(paciente)
--- cesfam/models.py
+++ cesfam/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class especialidad(models.Model):
+    idEspecialidad = models.IntegerField(primary_key=True, max_length=6, verbose_name='Id Categoria')
+    nombre = models.CharField(max_length=50, verbose_name='Especialidad')
+
+    def __str__(self):
+        return self.nombre
