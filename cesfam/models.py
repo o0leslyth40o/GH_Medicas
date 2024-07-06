51c767e175e635adc989efe3dc56ec5b806530cb
o0leslyth40o <102860687+o0leslyth40o@users.noreply.github.com>
2024-07-06 01:43:49 -0400

    elimine max length de especialidad

--- cesfam/models.py
+++ cesfam/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 class especialidad(models.Model):
-    idEspecialidad = models.IntegerField(primary_key=True, max_length=6, verbose_name='Id Categoria')
+    idEspecialidad = models.IntegerField(primary_key=True, verbose_name='Id Categoria')
     nombre = models.CharField(max_length=50, verbose_name='Especialidad')
 
     def __str__(self):
