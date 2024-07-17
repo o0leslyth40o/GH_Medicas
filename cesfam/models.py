3049469e16bfb1779607e77cbf17409f953d5db9
o0leslyth40o <102860687+o0leslyth40o@users.noreply.github.com>
2024-07-16 23:54:48 -0400

    incorpore campo imagen en modelo producto

--- cesfam/models.py
+++ cesfam/models.py
@@ -78,3 +78,7 @@ class atencion(models.Model):
         nombre = models.CharField(max_length=50)
         descripcion = models.TextField()
         stock = models.IntegerField()
+        imagen = models.ImageField(upload_to="productos", null=True)
+
+    def __str__(self):
+        return self.nombre
