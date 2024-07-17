3d2d4fca8b8abe59cd05b6101408d65d768cf6ec
o0leslyth40o <102860687+o0leslyth40o@users.noreply.github.com>
2024-07-17 01:44:40 -0400

    configure visualizacion en django

--- cesfam/admin.py
+++ cesfam/admin.py
@@ -1,14 +1,22 @@
 from django.contrib import admin
 from .models import especialidad, genero, medico, paciente, ficha, sector, agenda, atencion, producto
 
+class productoAdmin(admin.ModelAdmin):
+    list_display = ["nombre", "stock"]
+    list_editable = ["stock"]
+
+class medicoAdmin(admin.ModelAdmin):
+    list_display = ["rut","nombre", "appaterno", "especialidad", "imagen"]
+    search_fields = ["nombre"]
+    list_filter = ["especialidad"]
 
 # Register your models here.
 admin.site.register(especialidad)
 admin.site.register(genero)
-admin.site.register(medico)
+admin.site.register(medico, medicoAdmin)
 admin.site.register(paciente)
 admin.site.register(ficha)
 admin.site.register(sector)
 admin.site.register(agenda)
 admin.site.register(atencion)
-admin.site.register(producto)
+admin.site.register(producto, productoAdmin)
